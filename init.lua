7602e4ab8c55203eee3b1bcfbcb7b3968fb638e4
rsantan <ricardo.santander@mpzpm.mpg.de>
2025-10-30 12:10:43 +0100

    quarto installation

--- init.lua
+++ init.lua
@@ -351,6 +351,38 @@ require('lazy').setup({
     },
   },
 
+  {
+    'quarto-dev/quarto-nvim',
+    dependencies = {
+      'jmbuhr/otter.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('quarto').setup {
+        debug = false,
+        closePreviewOnExit = true,
+        lspFeatures = {
+          enabled = true,
+          chunks = 'curly',
+          languages = { 'r', 'python', 'julia', 'bash', 'html' },
+          diagnostics = {
+            enabled = true,
+            triggers = { 'BufWritePost' },
+          },
+          completion = {
+            enabled = true,
+          },
+        },
+        codeRunner = {
+          enabled = true,
+          default_method = 'slime',
+          ft_runners = {},
+          never_run = { 'yaml' },
+        },
+      }
+    end,
+  },
+
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
